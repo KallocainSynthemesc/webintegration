005d51c6909e91610a99691463a7e4e77cc69ffb
KallocainSynthemesc <74613762+KallocainSynthemesc@users.noreply.github.com>
2023-07-20 15:10:13 +0200

    fix wrong exercice again

--- src/components/SlidePresentation151.vue
+++ src/components/SlidePresentation151.vue
@@ -3,10 +3,13 @@
     <div class="heading">CSS - Sélecteurs d'attribut</div>
     <div class="content">
       <p>Implémentez cette page en utilisant diverses pseudo-classes.</p>
-      <div class="iFrame-container" data-link="exercices\effects\index.html">
+      <div
+        class="iFrame-container"
+        data-link="exercices\css\effects\index.html"
+      >
         <iframe
           ref="myIframe"
-          src="exercices\css\text\index.html"
+          src="exercices\css\effects\index.html"
           style="
             width: 100%;
             height: 85%;
